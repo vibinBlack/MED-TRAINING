4d2c1644ea7cbedabe17cbe9cb1f0e2795622296
Saikiran445 <82814717+Saikiran445@users.noreply.github.com>
2021-04-20 16:26:42 +0530

    zero

--- tests/basics/test_00_understanding_assert.py
+++ tests/basics/test_00_understanding_assert.py
@@ -6,24 +6,23 @@ notes = '''
 This lesson introduces the basic assert statement in python. assert is generally used to 'assert' the truth of an
 expression. It takes the form assert <expr>, <optional message>. If <expr> evaluates to False an AssertionError is raised with
 the <optional message>. If is evaluates to True, nothing happens.
-
  In the tests below, replace the blanks with values so that the resulting expression is True.
 '''
 
 
 def test_assert_true():
     #throws assertion error
-    assert ___  #This should be True -- replace ___ with True.
+    assert True  #This should be True -- replace ___ with True.
 
 def test_assert_true_with_message():
-    assert ___, "This is the failure message" # replace ___ with True to stop seeing the assertion error
+    assert True, "This is the failure message" # replace ___ with True to stop seeing the assertion error
 
 def test_assert_equality():
-    assert ___ == 2 + 5   #replace __ with the expected value
+    assert 7 == 2 + 5   #replace __ with the expected value
 
 #Fill in __ in the statements below to make the asserts succeed
 def test_make_assert_true_1():
-    assert __ > 7, "Fill in a value greater than 7"
+    assert 8 > 7, "Fill in a value greater than 7"
 
 #you can use the interpreter to find the value of 2**30
 def test_make_assert_true_2():
@@ -41,4 +40,3 @@ three_things_i_learnt = """
 """
 
 time_taken_minutes = __
-
