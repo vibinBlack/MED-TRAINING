6f0f827d80e028e297522f3e44e5be10b821126e
asshu1763 <mohdarshad7aa@gmail.com>
2021-12-02 15:45:44 +0530

    Updated code formatting using Pycharm

--- tests/Algorithms/algorithm_problems.py
+++ tests/Algorithms/algorithm_problems.py
@@ -395,6 +395,81 @@ def scrambled_words():
     return 0
 
 
+def shift_matrix():
+    # mat = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+    mat = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]
+    for i in mat:
+        for j in i:
+            print(j, end=" " * (4 - len(str(j))))
+        print("\n")
+    length = len(mat)
+    m = 0
+    n = length
+    out = []
+    for _ in range((length + 1) // 2):
+        k = []
+        for i in range(m, n):
+            for j in range(m, n):
+                if i == m:
+                    k.append(mat[i][j])
+        for i in range(m + 1, n):
+            for j in range(m, n):
+                if j == n - 1:
+                    k.append(mat[i][j])
+        for i in range(m, n):
+            for j in range(n - 2, m - 1, -1):
+                if i == n - 1:
+                    k.append(mat[i][j])
+        for i in range(n - 2, m, -1):
+            for j in range(m, n):
+                if j == m:
+                    k.append(mat[i][j])
+        out.append(k)
+        m = m + 1
+        n = n - 1
+    print()
+    final = []
+    shift = int(input("How many digits it should be shifted "))
+    for i in out:
+        if len(i) == 1:
+            shift = 0
+        if shift > len(i):
+            shift = shift % len(i)
+        for j in range(len(i)):
+            final.append(i[j - shift])
+    print()
+    dic = {}
+    i, j, min_i, min_j, max_i, max_j = 0, 0, 1, 0, length - 1, length - 1
+    status = "INC_J"
+    for k in range(0, length * length):
+        dic[(i, j)] = final[k]
+        if status == "INC_J":
+            j = j + 1
+            if j == max_j:
+                max_j = max_j - 1
+                status = "INC_I"
+        elif status == "INC_I":
+            i = i + 1
+            if i == max_i:
+                max_i = max_i - 1
+                status = "DEC_J"
+        elif status == "DEC_J":
+            j = j - 1
+            if j == min_j:
+                min_j = min_j + 1
+                status = "DEC_I"
+        elif status == "DEC_I":
+            i = i - 1
+            if i == min_i:
+                min_i = min_i + 1
+                status = "INC_J"
+    for i in range(length):
+        for j in range(length):
+            print(dic[(i, j)], end=" " * (4 - len(str(dic[(i, j)]))))
+        print("\n")
+    return 0
+
+
 def select_problem():
     """
     Select the Problem
@@ -403,7 +478,7 @@ def select_problem():
         \n3.  Calculator\n4.  Fibonacci Numbers\n5.  Triangle Pattern\n6.  Binary Search\
         \n7.  Linear Search\n8.  Palindrome\n9.  Prime Numbers\n10. Right Angled Triangle\
         \n11. Scrambled Words\n12. Extract data from CSV file\n13. Spiral Matrix\
-        \n14. Rotation of Matrix (90 degrees)\nC.  Close the Program\n")
+        \n14. Rotation of Matrix (90 degrees)\n15. Shift the Matrix digits\nC.  Close the Program\n")
     try:
         select = input("Select the above number for corresponding problem : ")
     except KeyboardInterrupt:
@@ -476,9 +551,14 @@ def select_problem():
     if select == '13':
         print("\n-------** Spiral Matrix **--------\n")
         spiral_matrix()
+
     if select == '14':
         print("\n-------** Rotation of Matrix **--------\n")
         rotation_of_matrix()
+
+    if select == '15':
+        print("\n-------** Shift Matrix Digits **--------\n")
+        shift_matrix()
 
     if select in ['c', 'C']:
         print("Bye..!!")
